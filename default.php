3d30aecfec213bff28707228d6a1fd97e6ba236b
Yurij2015 <nabor_2008@i.ua>
2020-04-23 15:05:07 +0300

    chat bot

---
+++ default.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ChatBots</title>
+</head>
+<body>
+<?php
+echo "ChatBots";
+?>
+<hr>
+<a href="emo-bot/bot/install">Установка</a>
+<hr>
+<a href="emo-bot/bot/admin">Админ</a>
+
+</body>
+</html>
